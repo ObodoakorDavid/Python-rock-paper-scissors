0652c80f3c890316ec5ae88418bf3bdb472f061e
ObodoakorDavid <obodoakordavid@outlook.com>
2022-08-09 01:02:10 -1100

    added a while loop for repeating plays

--- Rps.py
+++ Rps.py
@@ -1,36 +1,40 @@
+from ast import While
 import random
 
-print( 'Rock, Paper, Scissors Game')
-option = input('Choose one:').lower()
-# print(option)
+while True:
+    rockPaperScissors = ['rock', 'paper', 'scissors']
+    randomNumber = int(random.random() * 3)
+    computerOption = rockPaperScissors[randomNumber]
+    print( 'Rock, Paper, Scissors Game')
+    option = ''
 
-randomNumber = int(random.random() * 3)
+    while option not in rockPaperScissors:
+        option = input('Choose one:').lower()
 
-rockPaperScissors = ['rock', 'paper', 'scissors']
-computerOption = rockPaperScissors[randomNumber]
+    result = f'You chose {option} and computer chose {computerOption}'
 
-result = f'You chose {option} and computer chose {computerOption}'
+    if option == 'rock' and computerOption == 'scissors':
+        print(result)
+        print(f'You win because {option} wins {computerOption}')
+    elif option == 'paper' and computerOption == 'rock':
+        print(result)
+        print(f'You win because {option} wins {computerOption}')
+    elif option == 'scissors' and computerOption == 'paper':
+        print(result)
+        print(f'You win because {option} wins {computerOption}')
+    elif option ==  computerOption:
+        print(result)
+        print(f"it's a tie")
+    else:
+        print(result)
+        print(f'You lose because {computerOption} wins {option}')
+        
+    playAgain = input("Play again? (y/n):").lower()
 
-# print(rockPaperScissors[randomNumber])
+    if playAgain != 'y':
+        break
 
-if option != 'rock' and option != 'paper' and option != 'scissors':
-    print(f"You didn't pick a valid option")
-elif option == 'rock' and computerOption == 'scissors':
-    print(result)
-    print(f'You win because {option} wins {computerOption}')
-elif option == 'paper' and computerOption == 'rock':
-    print(result)
-    print(f'You win because {option} wins {computerOption}')
-elif option == 'scissors' and computerOption == 'paper':
-    print(result)
-    print(f'You win because {option} wins {computerOption}')
-elif option ==  computerOption:
-    print(result)
-    print(f"it's a tie")
-else:
-    print(result)
-    print(f'You lose because {computerOption} wins {option}')
-
+print('Bye!!')
 
 
 
